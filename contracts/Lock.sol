a5667976e47e4779c23557d699de9245878885c5
Huáscar Miranda <huascarm@gmail.com>
2023-03-20 18:03:57 -0400

    typescript project

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,14 +4,31 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Pass3Manager {
-    struct Pair {
-        string user;
-        string password;
-    }
-    mapping(address => Pair) private credentials;
+contract Lock {
+    uint public unlockTime;
+    address payable public owner;
 
-    function save_pair(Pair memory _pair) public {
-        credentials[msg.sender] = _pair;
+    event Withdrawal(uint amount, uint when);
+
+    constructor(uint _unlockTime) payable {
+        require(
+            block.timestamp < _unlockTime,
+            "Unlock time should be in the future"
+        );
+
+        unlockTime = _unlockTime;
+        owner = payable(msg.sender);
+    }
+
+    function withdraw() public {
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "You aren't the owner");
+
+        emit Withdrawal(address(this).balance, block.timestamp);
+
+        owner.transfer(address(this).balance);
     }
 }
---
+++ hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: process.env.POLYGON_RPC,
+      accounts: [process.env.PRIVATE_KEY || "default"],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
+};
+
+export default config;
